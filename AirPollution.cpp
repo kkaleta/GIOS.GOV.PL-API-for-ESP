23f7a7143493ed190adcb294f1c7b78d9faf29d8
kkaleta <kamilkaleta77@gmail.com>
2019-01-02 14:39:37 +0100

    Update AirPollution.cpp

--- AirPollution.cpp
+++ AirPollution.cpp
@@ -1,18 +1,9 @@
-/*The MIT License (MIT)
-Copyright (c) 2018 Kamil Kaleta. All right reserved.
-http://theveel.com/?p=482&preview=true
+/*
+The MIT License (MIT) Copyright (c) 2018 Kamil Kaleta. All right reserved. http://theveel.com/?p=482
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-Lesser General Public License for more details.
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version. This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+This library provide access to specific air parameters, especially air pollution PM (PM - Particulate Matters), from gios.gov.pl
 */
 
 #if defined(ESP8266)
@@ -33,11 +24,10 @@ int sensorIndex;
 String tempId;
 
 //#######################################
-bool showOutput = true;
+bool showOutput = false;
 //#######################################
 
 void AirPollution::getSensors(int locId){
-	//pjp-api/rest/station/sensors/'.$key;
 	stationSensorsIds[0]="-2";
 	stationSensorsIds[1]="-3";
 	isSensors=true;
@@ -60,17 +50,19 @@ void AirPollution::getData(int sensorId){
 	tempId="";
 	isData=true;
 	isSensors=false;
-	Serial.print(">>sensorID:");
-	Serial.println(sensorId);
-	doUpdate("/pjp-api/rest/data/getData/" + (String)sensorId);	
+	if(showOutput)
+	Serial.println("*AirPollution::getData sensorID:"+sensorID);
+	doUpdate("/pjp-api/rest/data/getData/" + (String)sensorId);
+	
 }
 
+
 void AirPollution::doUpdate(String url) {
-  JsonStreamingParser parser;
-  parser.setListener(this);
-  yield();
-  WiFiClient client;
-  const int httpPort = 80;
+	  JsonStreamingParser parser;
+	  parser.setListener(this);
+	  yield();
+	  WiFiClient client;
+	  const int httpPort = 80;
 
   int i =0;
   while(!client.connect("api.gios.gov.pl", httpPort)){
@@ -79,12 +71,13 @@ void AirPollution::doUpdate(String url) {
 	  if(i>10)
 		  ESP.restart();
 		  // return;
-	  Serial.println("!!! no api gios.gov.pl");
+		  if(showOutput)
+	  Serial.println("*ERROR! no api.gios.gov.pl");
 	  
   }
   delay(1);
 if(showOutput){
-  Serial.print("Requesting URL: ");
+  Serial.print("*Requesting URL: ");
   Serial.println(url);
 }
   // This will send the request to the server
@@ -108,7 +101,7 @@ if(showOutput){
   int size = 0;
   client.setNoDelay(false);
   yield();
-  while(client.connected()) {
+
     while((size = client.available()) > 0) {
       c = client.read();
       if (c == '{' || c == '[') {
@@ -116,11 +109,11 @@ if(showOutput){
       }
       if (isBody) {
         parser.parse(c);
+		delay(1);
+		// Serial.print(c);
       }
-		  delay(1);
     }
-	delay(1);
-	  }
+
   delay(1);
 }
 
@@ -132,8 +125,8 @@ void AirPollution::key(String key) {
 	  currentKey = String(key);
 //#######################################  
 if(showOutput){
-     	Serial.print("currentParent: ");
- 	Serial.print(currentParent);	
+    Serial.print("*currentParent: ");
+	Serial.print(currentParent);	
 	Serial.print(" currentKey: ");
 	Serial.println(currentKey);
 }
@@ -144,7 +137,7 @@ void AirPollution::value(String value) {
 	
 //#########################################	
 if(showOutput){
-	Serial.print("currentValue! ");
+	Serial.print("*currentValue! ");
 	Serial.println(value);
 }
 //########################################	
@@ -154,42 +147,49 @@ if(showOutput){
 		if (currentKey == "id"){ tempId=value;}
 		
 		if(currentKey == "idParam" && value=="69"){ //PM2.5
-			Serial.print("pm2, id 69=");
+			if(showOutput){
+			Serial.print("*pm2, id 69=");
 			Serial.println(tempId);
+			}
 			stationSensorsIds[0]=tempId;
 		}
 		if(currentKey == "idParam" && value=="3"){ //PM10
-				Serial.print("pm10, id 3=");
+		if(showOutput){
+				Serial.print("*pm10, id 3=");
 		Serial.println(tempId);
+		}
 			stationSensorsIds[1]=tempId;
 		}
 	}
 //AQI
 	if(currentParent=="stIndexLevel"){
 		if(currentKey=="id"){
-			Serial.print("####stIndexLevel id: ");
+			if(showOutput){
+			Serial.print("*stIndexLevel id: ");
 			Serial.println(value);
+			}
 			
 		}
 		else if(currentKey=="indexLevelName"){
-			Serial.print("####indexLevelName: ");
+			if(showOutput){
+			Serial.print("*indexLevelName: ");
 			Serial.println(value);
-			
+			}
 		}		
 	}
 //.AQI
 if(isData){
-		//SINGLE MEASURMENT
+			//SINGLE MEASURMENT
 		if (currentKey =="value" && value !="null" && measureNotFound){// && measureNotFound && notProperMeasureCnt<6){
 				if(showOutput){
-						Serial.print("####measure: ");
+						Serial.print("*measure: ");
 						Serial.println(value);
 					}		
 				stationSensorsValue=value;
 				measureNotFound=false;
 				notProperMeasureCnt=-1;
 		}
-		//.SINGLE MEASURMENT	
+			//.SINGLE MEASURMENT	
 	}
 }
 
@@ -199,10 +199,7 @@ void AirPollution::endObject() {currentParent = "";}
 void AirPollution::endDocument() {}
 void AirPollution::startArray() {}
 
-
-
- String AirPollution::getStationSensorsIds(int sensorIndex){
-	 // getSensors(station);
+String AirPollution::getStationSensorsIds(int sensorIndex){
 	 return stationSensorsIds[sensorIndex];
  }
  
